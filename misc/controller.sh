9972ce22cacc85b22e95c72b6325b468bbfebbf4
Carter Frost <carter90@gmail.com>
2022-01-06 23:32:02 -0800

    misc/controller.sh: added comment for faster startup

--- misc/controller.sh
+++ misc/controller.sh
@@ -6,8 +6,8 @@ sleep 1
 source /opt/ros/noetic/setup.bash
 roscore &
 cd cabrillo_rov/
-git pull
-catkin_make
+git pull    # Comment out for quicker rov startup
+catkin_make # Comment out for quicker rov startup
 source ./devel/setup.bash
 sleep 5
 roslaunch ./launch/rov.launch &
