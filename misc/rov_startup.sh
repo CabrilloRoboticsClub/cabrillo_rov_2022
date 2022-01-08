081ee5be483c1670a8f3f0e09aa6b0067abd49f8
Carter Frost <carter90@gmail.com>
2022-01-08 14:30:47 -0800

    misc/rov_startup.sh: update commented instructions for script new name

--- misc/rov_startup.sh
+++ misc/rov_startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #make a on boot cronjob with `chrontab -e`
-# then paste at the bottom `@reboot /home/ubuntu/cabrillo_rov/misc/controller.sh`
+# then paste at the bottom `@reboot /home/ubuntu/cabrillo_rov/misc/rov_startup.sh`
 #sudo pigpiod # put this line in a cronjob under root on boot or create a service
 sleep 1
 source /opt/ros/noetic/setup.bash
