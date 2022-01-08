0ea0662a6faeead8630a141af459de69ebce97dd
Carter Frost <carter90@gmail.com>
2022-01-08 14:58:05 -0800

    misc/rov_startup.sh: add absolute path for repo in script

--- misc/rov_startup.sh
+++ misc/rov_startup.sh
@@ -5,7 +5,7 @@
 sleep 1
 source /opt/ros/noetic/setup.bash
 roscore &
-cd cabrillo_rov/
+cd /home/ubuntu/cabrillo_rov/
 git pull    # Comment out for quicker rov startup
 catkin_make # Comment out for quicker rov startup
 source ./devel/setup.bash
