5dc212f01e9e52de534a98c7162b4f52deb82d1e
cTurtle98 <ciaran@cturtle98.com>
2022-04-06 12:11:33 -0700

    startup fscript for ros

--- misc/rov_startup.sh
+++ misc/rov_startup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#make a on boot cronjob with `chrontab -e`
-# then paste at the bottom `@reboot /home/ubuntu/cabrillo_rov/misc/rov_startup.sh`
-#sudo pigpiod # put this line in a cronjob under root on boot or create a service
+#
+# execution of this file will be added to the crontab by running the ansible playbook ansible_playbook_rov.yml
+#
 sleep 1
 source /opt/ros/noetic/setup.bash
 roscore &
 cd /home/ubuntu/cabrillo_rov/
-git pull    # Comment out for quicker rov startup
-catkin_make # Comment out for quicker rov startup
+# git pull    # Comment out for quicker rov startup
+# catkin_make # Comment out for quicker rov startup
 source ./devel/setup.bash
 sleep 5
 roslaunch ./launch/rov.launch &
