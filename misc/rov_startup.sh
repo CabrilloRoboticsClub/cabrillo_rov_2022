8e5d338949f2bc54d5ca91ce9426fdcfcabd0992
cTurtle98 <ciaran@cturtle98.com>
2022-04-06 16:10:13 -0700

    update startup script

--- misc/rov_startup.sh
+++ misc/rov_startup.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #
-# execution of this file will be added to the crontab by running the ansible playbook ansible_playbook_rov.yml
+# execution of this is added to ubuntu user's crontab by running the ansible playbook "ansible_playbook_rov.yml"
 #
 sleep 1
 source /opt/ros/noetic/setup.bash
 roscore &
 cd /home/ubuntu/cabrillo_rov/
-# git pull    # Comment out for quicker rov startup
-# catkin_make # Comment out for quicker rov startup
+catkin_make
 source ./devel/setup.bash
 sleep 5
 roslaunch ./launch/rov.launch &
