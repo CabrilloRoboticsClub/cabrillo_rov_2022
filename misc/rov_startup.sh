6d09f761fb21282904cfc00c18fffa2791eaf588
cTurtle98 <ciaran@cturtle98.com>
2022-04-06 18:38:58 -0700

    enable

--- misc/rov_startup.sh
+++ misc/rov_startup.sh
@@ -6,8 +6,8 @@ sleep 1
 source /opt/ros/noetic/setup.bash
 roscore &
 cd /home/ubuntu/cabrillo_rov/
-# git pull    # Comment out for quicker rov startup
-# catkin_make # Comment out for quicker rov startup
+git pull    # Comment out for quicker rov startup
+catkin_make # Comment out for quicker rov startup
 source ./devel/setup.bash
 sleep 5
 roslaunch ./launch/rov.launch &
