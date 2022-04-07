8e2355265a913c9fc1f337cb7722979adbf63339
cTurtle98 <ciaran@cturtle98.com>
2022-04-06 18:39:10 -0700

    fix hostname

--- misc/shore_startup.sh
+++ misc/shore_startup.sh
@@ -6,6 +6,6 @@ cd cabrillo_rov/
 git pull    # Comment out for quicker rov startup
 catkin_make # Comment out for quicker rov startup
 source ./devel/setup.bash
-export ROS_MASTER_URI=http://$(getent hosts earle-s1.local | cut -d " " -f1):11311
+export ROS_MASTER_URI=http://$(getent hosts hydrozoa | cut -d " " -f1):11311
 sleep 1
 roslaunch ./launch/shore.launch
