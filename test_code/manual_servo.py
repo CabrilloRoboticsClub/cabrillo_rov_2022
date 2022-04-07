8037f334dcfd0713d7e8723816eedd2a5a77e546
cTurtle98 <ciaran@cturtle98.com>
2022-04-07 02:40:56 -0700

    why does this make a difference

--- test_code/manual_servo.py
+++ test_code/manual_servo.py
@@ -21,14 +21,14 @@ claw4 = servo.Servo(pca.channels[7])
 # set all the thrusters to zero to init
 
 thrust1.fraction = 0
-thrust2.fraction = 0
-thrust3.fraction = 0
-thrust4.fraction = 0
-
 thrust1.angle = 180
+thrust2.fraction = 0
 thrust2.angle = 180
+thrust3.fraction = 0
 thrust3.angle = 180
+thrust4.fraction = 0
 thrust4.angle = 180
+
 
 # that section above works and I get the 2 init beeps
 
