36b397f50d85b83d9773ab4189ec16daeac131d2
cTurtle98 <ciaran@cturtle98.com>
2022-04-07 02:16:17 -0700

    bla

--- test_code/manual_servo.py
+++ test_code/manual_servo.py
@@ -18,6 +18,8 @@ claw2 = servo.Servo(pca.channels[5])
 claw3 = servo.Servo(pca.channels[6])
 claw4 = servo.Servo(pca.channels[7])
 
+# set all the thrusters to zero to init
+
 thrust1.angle = 180
 thrust1.fraction = 0
 
@@ -29,3 +31,18 @@ thrust3.fraction = 0
 
 thrust4.angle = 180
 thrust4.fraction = 0
+
+
+# this should start them moving
+
+thrust1.angle = 180
+thrust1.fraction = 1
+
+thrust2.angle = 180
+thrust2.fraction = 1
+
+thrust3.angle = 180
+thrust3.fraction = 1
+
+thrust4.angle = 180
+thrust4.fraction = 1
