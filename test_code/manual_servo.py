9457f33463ba0f8cb2839d13f35bb20b0ab1e7d7
cTurtle98 <ciaran@cturtle98.com>
2022-04-07 02:44:40 -0700

    lets try the claws. thrustes dont work

--- test_code/manual_servo.py
+++ test_code/manual_servo.py
@@ -18,31 +18,34 @@ claw2 = servo.Servo(pca.channels[5])
 claw3 = servo.Servo(pca.channels[6])
 claw4 = servo.Servo(pca.channels[7])
 
-print("thrust fraction = 0, angle = 180")
+# print("thrust fraction = 0, angle = 180")
 
-thrust1.fraction = 0
-thrust1.angle = 180
-thrust2.fraction = 0
-thrust2.angle = 180
-thrust3.fraction = 0
-thrust3.angle = 180
-thrust4.fraction = 0
-thrust4.angle = 180
+# thrust1.fraction = 0
+# thrust1.angle = 180
+# thrust2.fraction = 0
+# thrust2.angle = 180
+# thrust3.fraction = 0
+# thrust3.angle = 180
+# thrust4.fraction = 0
+# thrust4.angle = 180
 
 
-# that section above works and I get the 2 init beeps
-print("sleep 5")
-time.sleep(5)
-print("fraction 1")
-thrust1.fraction = 1
-thrust2.fraction = 1
-thrust3.fraction = 1
-thrust4.fraction = 1
+# # that section above works and I get the 2 init beeps
+# print("sleep 5")
+# time.sleep(5)
+# print("fraction 1")
+# thrust1.fraction = 1
+# thrust2.fraction = 1
+# thrust3.fraction = 1
+# thrust4.fraction = 1
 
-print("angle 150")
-thrust1.angle = 150
-thrust2.angle = 150
-thrust3.angle = 150
-thrust4.angle = 150
+# print("angle 150")
+# thrust1.angle = 150
+# thrust2.angle = 150
+# thrust3.angle = 150
+# thrust4.angle = 150
 
-time.sleep(30)
+# time.sleep(30)
+
+claw1.angle = 0
+claw1.angle = 180
