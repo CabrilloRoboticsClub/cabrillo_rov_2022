c4570ed6040c537c8cf7de249e6707247ac379f5
cTurtle98 <ciaran@cturtle98.com>
2022-04-07 02:18:35 -0700

    how about this

--- test_code/manual_servo.py
+++ test_code/manual_servo.py
@@ -32,17 +32,18 @@ thrust3.fraction = 0
 thrust4.angle = 180
 thrust4.fraction = 0
 
+time.sleep(3)
 
 # this should start them moving
 
-thrust1.angle = 200
+thrust1.angle = 90
 #thrust1.fraction = 1
 
-thrust2.angle = 200
+thrust2.angle = 90
 #thrust2.fraction = 1
 
-thrust3.angle = 200
+thrust3.angle = 90
 #thrust3.fraction = 1
 
-thrust4.angle = 200
+thrust4.angle = 90
 #thrust4.fraction = 1
