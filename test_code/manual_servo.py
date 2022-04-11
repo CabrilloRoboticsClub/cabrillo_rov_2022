71af351a334cf88f8bbbb2851d07a3cdc2d9de56
cTurtle98 <ciaran@cturtle98.com>
2022-04-10 17:58:22 -0700

    be more explicite

--- test_code/manual_servo.py
+++ test_code/manual_servo.py
@@ -6,19 +6,19 @@ pwm_hat = ServoKit(channels=16)
 # esc valid input raneg 1100 to 1900
 # using 100 dif for testing
 thrust1 = pwm_hat.continuous_servo[0]
-thrust1.set_pulse_width_range(1400, 1600)
+thrust1.set_pulse_width_range(min_pulse=1400, max_pulse=1600)
 thrust1.throttle = 0
 
 thrust2 = pwm_hat.continuous_servo[1]
-thrust2.set_pulse_width_range(1400, 1600)
+thrust2.set_pulse_width_range(min_pulse=1400, max_pulse=1600)
 thrust2.throttle = 0
 
 thrust3 = pwm_hat.continuous_servo[2]
-thrust3.set_pulse_width_range(1400, 1600)
+thrust3.set_pulse_width_range(min_pulse=1400, max_pulse=1600)
 thrust3.throttle = 0
 
 thrust4 = pwm_hat.continuous_servo[3]
-thrust4.set_pulse_width_range(1400, 1600)
+thrust4.set_pulse_width_range(min_pulse=1400, max_pulse=1600)
 thrust4.throttle = 0
 
 time.sleep(5)
