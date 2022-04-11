fdce5bd657c3d8803d16002e5a37f6e459af7ce1
cTurtle98 <ciaran@cturtle98.com>
2022-04-10 17:38:08 -0700

    make all statements use continuous_servo

--- test_code/manual_servo.py
+++ test_code/manual_servo.py
@@ -5,15 +5,15 @@ pwm_hat = ServoKit(channels=16)
 
 # esc valid input raneg 1100 to 1900
 # using 100 dif for testing
-pwm_hat.servo[0].set_pulse_width_range(1400, 1600)
-pwm_hat.continuous_servo[0].throttle=0
+pwm_hat.continuous_servo[0].set_pulse_width_range(1400, 1600)
+pwm_hat.continuous_servo[0].throttle = 0
 
-pwm_hat.servo[1].set_pulse_width_range(1400, 1600)
-pwm_hat.continuous_servo[1].throttle=0
+pwm_hat.continuous_servo[1].set_pulse_width_range(1400, 1600)
+pwm_hat.continuous_servo[1].throttle = 0
 
-pwm_hat.servo[2].set_pulse_width_range(1400, 1600)
-pwm_hat.continuous_servo[2].throttle=0
+pwm_hat.continuous_servo[2].set_pulse_width_range(1400, 1600)
+pwm_hat.continuous_servo[2].throttle = 0
 
-pwm_hat.servo[3].set_pulse_width_range(1400, 1600)
-pwm_hat.continuous_servo[3].throttle=0
+pwm_hat.continuous_servo[3].set_pulse_width_range(1400, 1600)
+pwm_hat.continuous_servo[3].throttle = 0
 
