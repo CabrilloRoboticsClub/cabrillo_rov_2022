5d774325252d8d107714338db1169bdf55d860be
cTurtle98 <ciaran@cturtle98.com>
2022-04-10 17:41:31 -0700

    try to move thrust1

--- test_code/manual_servo.py
+++ test_code/manual_servo.py
@@ -5,8 +5,9 @@ pwm_hat = ServoKit(channels=16)
 
 # esc valid input raneg 1100 to 1900
 # using 100 dif for testing
-pwm_hat.continuous_servo[0].set_pulse_width_range(1400, 1600)
-pwm_hat.continuous_servo[0].throttle = 0
+thrust1 = pwm_hat.continuous_servo[0]
+thrust1.set_pulse_width_range(1400, 1600)
+thrust1.throttle = 0
 
 pwm_hat.continuous_servo[1].set_pulse_width_range(1400, 1600)
 pwm_hat.continuous_servo[1].throttle = 0
@@ -17,3 +18,10 @@ pwm_hat.continuous_servo[2].throttle = 0
 pwm_hat.continuous_servo[3].set_pulse_width_range(1400, 1600)
 pwm_hat.continuous_servo[3].throttle = 0
 
+time.sleep(5)
+
+thrust1.throttle = 0.8
+
+time.sleep(5)
+
+thrust1.throttle = 0
