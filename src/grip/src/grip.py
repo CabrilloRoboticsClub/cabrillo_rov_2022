8eaad95645b81cf5973e26f190f9b147bc670cde
cTurtle98 <ciaran@cturtle98.com>
2022-04-20 01:39:28 -0700

    add comments

--- src/grip/src/grip.py
+++ src/grip/src/grip.py
@@ -19,18 +19,23 @@ class Grip:
     # 14 rear horizontal
     # 15 rear vertical
 
-    self.actuation_range = 180
+    # pwm value at claw fully closed
     self.pwm_min = 1000
+    # pwm value at claw fullly open
     self.pwm_max = 2000
+    # pwm value at claw halfway open
+    self.pwm_base = 1500
 
-    #self.base = 1500
-    #self.angle = 90
-    #self.grippers = None
-    # 500-2500 if we want 180 deg, 1000-2000 for 90 deg
-    self.max_from_base = 500  # max_forward = 2000, max_backward = 1000
+    # angle value for claw closed
+    self.angle_min = 0
+    # angle value for claw open
+    self.angle_max = 180
+    # angle value at claw halfway open
+    self.angle_base = 90
+
 
   def move(self, data, which_gripper=0):
-    servos.servo[self.gripper_pins[which_gripper]].angle = self.angle + (data * self.angle)
+    kit.servo[self.gripper_pins[which_gripper]].angle = self.angle + (data * self.angle)
     
 
   # Initializes everything
@@ -38,26 +43,15 @@ class Grip:
     # instanciate servokit for the 16 channel servo board
     kit = ServoKit(channels = 16)
 
-    # front vertical
-    kit.servo[self.gripper_pins[0]].actuation_range = self.actuation_range
-    kit.servo[self.gripper_pins[0]].set_pulse_width_range(self.pwm_min, self.pwm_max)
+    # run through the gripper pins and set them up
+    for pin in self.gripper_pins:
+      # set the actuation range for the servokit library
+      kit.servo[pin].actuation_range = self.angle_max - self.angle_min
+      # set the pwm range the servos respond to
+      kit.servo[pin].set_pulse_width_range(self.pwm_min, self.pwm_max)
+      # initialise the claw servos half open
+      kit.servo[pin].angle = self.base_angle
 
-    # front horizontal
-    kit.servo[self.gripper_pins[1]].actuation_range = self.actuation_range
-    kit.servo[self.gripper_pins[1]].set_pulse_width_range(self.pwm_min, self.pwm_max)
-
-    # rear horizontal
-    kit.servo[self.gripper_pins[2]].actuation_range = self.actuation_range
-    kit.servo[self.gripper_pins[2]].set_pulse_width_range(self.pwm_min, self.pwm_max)
-
-    # rear vertical
-    kit.servo[self.gripper_pins[3]].actuation_range = self.actuation_range
-    kit.servo[self.gripper_pins[3]].set_pulse_width_range(self.pwm_min, self.pwm_max)
-
-    #servos.servo[self.gripper_pins[0]].set_pulse_width_range(self.base - self.max_from_base, self.base + self.max_from_base)
-    #servos.servo[self.gripper_pins[1]].set_pulse_width_range(self.base - self.max_from_base, self.base + self.max_from_base)
-    #servos.servo[0].angle = self.angle
-    #servos.servo[1].angle = self.angle
     rospy.Subscriber("cmd_gripper1", Float32, self.move, 0)
     rospy.Subscriber("cmd_gripper2", Float32, self.move, 1)
     rospy.spin()
