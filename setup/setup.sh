4b9f55e0f6feb268fc31abdb218be77eaa0a546b
CabrilloRobotics-3 <cabrillorobotics@gmail.com>
2022-05-03 22:51:04 -0700

    building CabrilloRobotics-3 usb drive

--- setup/setup.sh
+++ setup/setup.sh
@@ -6,8 +6,8 @@ sudo apt upgrade -y
 
 sudo apt autoremove -y
 
-sudo apt install python3-pip sshpass
+sudo apt install -y python3-pip sshpass
 
-pip3 install ansible
+sudo pip3 install ansible
 
 ansible-playbook -i ansible_inventory.yml ansible_playbook.yml
