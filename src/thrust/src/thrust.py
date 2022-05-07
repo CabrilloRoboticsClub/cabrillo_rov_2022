b9452adbab613c2699c3beff0761fce46ab71b9d
Carter Frost <carter90@gmail.com>
2022-05-06 22:12:20 -0600

    src/thrust/src/thrust.py: added light class just needs the pin #, pi arg

--- src/thrust/src/thrust.py
+++ src/thrust/src/thrust.py
@@ -10,7 +10,8 @@ import pigpio
 
 
 class Thrust:
-  def __init__(self):
+  def __init__(self, pi):
+    self.pi = pi
     rospy.init_node('thrust')
     self.thruster_pins = [20, 19, 24, 27]
     self.base = 1500
@@ -20,15 +21,15 @@ class Thrust:
     self.overridden = False
     self.scaler = self.max_from_base / 3.0  # @TODO dynamic reconfigure this
 
+
   # @sync #TODO: add blocking so if we get a joy/command msg we will ignore it
   def move(self, data):
     if not self.overridden:
-      data.linear.x  # forward/back
-      data.angular.x  # "roll" rotate side/side # should not do this much/at all, currently ignored
-      data.linear.z  # up/down
-      data.angular.z  # "yaw" rotate left/right
+      # data.linear.x  # forward/back
+      # data.angular.x  # "roll" rotate side/side # should not do this much/at all, currently ignored
+      # data.linear.z  # up/down
+      # data.angular.z  # "yaw" rotate left/right
 
-      # math.cos()
       # @NOTE: data.angular.x is already from -1 -> 1 so can just use it as such
       self.thrust(self.thruster_pins[0], self.base + ((data.linear.x - data.angular.z) * self.scaler))
       self.thrust(self.thruster_pins[1], self.base + ((data.linear.x + data.angular.z) * self.scaler))
@@ -46,17 +47,33 @@ class Thrust:
 
   # Initializes everything
   def run(self):
-    pi = pigpio.pi()
-    pi.set_servo_pulsewidth(self.thruster_pins[0], self.base)
-    pi.set_servo_pulsewidth(self.thruster_pins[1], self.base)
-    pi.set_servo_pulsewidth(self.thruster_pins[2], self.base)
-    pi.set_servo_pulsewidth(self.thruster_pins[3], self.base)
-    self.thrust = pi.set_servo_pulsewidth
+    self.pi.set_servo_pulsewidth(self.thruster_pins[0], self.base)
+    self.pi.set_servo_pulsewidth(self.thruster_pins[1], self.base)
+    self.pi.set_servo_pulsewidth(self.thruster_pins[2], self.base)
+    self.pi.set_servo_pulsewidth(self.thruster_pins[3], self.base)
+    self.thrust = self.pi.set_servo_pulsewidth
     rospy.Subscriber("cmd_vel", Twist, self.move)
     rospy.Subscriber("override", Bool, self.override)
     rospy.spin()
 
 
+class Light:
+  def __init__(self, pi):
+    self.pi = pi
+    self.light_pin = None
+
+  def light_message(self, data):
+    self.pi.write(self.light_pin, int(data.data))  # 0(False/Off) / 1(True/On)
+
+  def run(self):
+    rospy.Subscriber("light", Bool, self.light_message)
+
+
 if __name__ == '__main__':
-  t = Thrust()
+  pi = pigpio.pi()
+  t = Thrust(pi)
+  l = Light(pi)
+
+  # after you figure out which pin you want to use and put it in light_pin then comment this back in
+  # l.run()
   t.run()
