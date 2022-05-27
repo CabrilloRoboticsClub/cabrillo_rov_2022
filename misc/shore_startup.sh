c9e528881f3ba6e7f1596a7d120b64446c1aba1f
ciaran-dell <cabrillorobotics@gmail.com>
2022-05-26 21:56:58 -0700

    comment out earle ros uri uncomment hydrozoa ros uri

--- misc/shore_startup.sh
+++ misc/shore_startup.sh
@@ -6,8 +6,8 @@ cd ~/cabrillo_rov/
 git pull    # Comment out for quicker rov startup
 catkin_make # Comment out for quicker rov startup
 source ./devel/setup.bash
-#export ROS_MASTER_URI=http://$(getent hosts hydrozoa | cut -d " " -f1):11311
-export ROS_MASTER_URI='http://earle-s1.local:11311'
+export ROS_MASTER_URI=http://$(getent hosts hydrozoa | cut -d " " -f1):11311
+#export ROS_MASTER_URI='http://earle-s1.local:11311'
 export ROS_HOSTNAME=$HOSTNAME.local
 sleep 1
 roslaunch ./launch/shore.launch
