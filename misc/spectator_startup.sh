a8209a00ceff803e08c6c5fffcda6798502c708d
ciaran-dell <cabrillorobotics@gmail.com>
2022-06-06 18:45:09 -0700

    spectator startup is more of an init now

--- misc/spectator_startup.sh
+++ misc/spectator_startup.sh
@@ -8,8 +8,8 @@ cd ~/cabrillo_rov/
 git pull
 catkin_make
 source ./devel/setup.bash
-export ROS_MASTER_URI='http://earle-s1.local:11311'
+export ROS_MASTER_URI='http://hydrozoa.local:11311'
 export ROS_HOSTNAME=$HOSTNAME.local
 sleep 1
 
-rqt --perspective-file misc/spectator.perspective
+#rqt --perspective-file misc/spectator.perspective
